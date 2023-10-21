718729f6b8c57f0ed269aa86f58f0a2c62d79e3a
Ygor Torres <ygorftf@gmail.com>
2023-10-21 08:40:10 -0300

    Data transaction in the database using setAutoCommit, commit and rollback methods.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import db.DB;
+import db.DbException;
 import db.DbIntegrityException;
 
 import java.sql.*;
@@ -11,24 +12,31 @@ public class Main {
     public static void main(String[] args) {
 
         Connection conn = null;
-        PreparedStatement st = null;
+        Statement st = null;
 
         try{
             conn = DB.getConnection();
 
-            st = conn.prepareStatement(
-                    "DELETE FROM department " +
-                    "WHERE " +
-                    "Id = ?");
+            conn.setAutoCommit(false);
 
-            st.setInt(1,4);
+            st = conn.createStatement();
 
-            int rowsAffected = st.executeUpdate();
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 
-            System.out.println("Done! rows affected: " + rowsAffected);
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("rows1 = " + rows1);
+            System.out.println("rows2 = " + rows2);
 
         } catch (SQLException e) {
-            throw new DbIntegrityException(e.getMessage());
+            try {
+                conn.rollback();
+                throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+            } catch (SQLException e1){
+                throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
+            }
         } finally {
             DB.closeStatement(st);
             DB.closeConnection();
